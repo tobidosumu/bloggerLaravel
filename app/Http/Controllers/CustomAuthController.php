313e4a103a7aa0cfb829b879b16437987eb73311
tobidosumu <dosunmumishael@gmail.com>
2023-02-14 19:52:29 +0100

    update: add more validation rules

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -24,7 +24,8 @@ class CustomAuthController extends Controller
             return redirect()->intended('/')->withSuccess('Signed in');
         }
   
-        return redirect("login")->withSuccess('Login details are not valid');
+        // return redirect("login")->withSuccess('Login details are not valid');
+        return redirect('login')->with('error', 'These credentials do not match our records.');
     }
 
     public function registration()
@@ -32,19 +33,36 @@ class CustomAuthController extends Controller
         return view('auth.register');
     }
       
+    // Register page validation method
     public function customRegistration(Request $request)
-    {  
+    {
         $request->validate([
-            'name' => 'required|min:3',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:8',
+            'name' => 'required|min:3|max:20',
+            'email' => 'required|email|min:2|unique:users',
+            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).+$/',
+            'password_confirmation' => 'required_with:password|same:password',
+        ], [
+            'name.required' => 'Name cannot be empty.',
+            'name.min' => 'Name must be at least :min characters.',
+            'name.max' => 'The name may not be greater than :max characters.',
+
+            'email.required' => 'Email cannot be empty.',
+            'email.email' => 'The email must be a valid email address.',
+            'email.min' => 'The email must be at least :min characters.',
+            'email.unique' => 'The email has already been taken.',
+
+            'password.required' => 'Password cannot be empty.',
+            'password.min' => 'Password must be at least :min characters.',
+            'password.regex' => 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+            'password_confirmation.required_with' => 'Password confirmation field is required.',
+            'password_confirmation.same' => 'The password confirmation does not match the password.'
         ]);
-           
+    
         $data = $request->all();
         $check = $this->create($data);
-         
-        return redirect('dashboard')->withSuccess('You are signed-in');
-    }
+    
+        return redirect('dashboard');
+    }    
 
     public function create(array $data)
     {

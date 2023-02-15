5eeea703f39bfeebd0febc6d2ee629a127b41670
tobidosumu <dosunmumishael@gmail.com>
2023-02-16 00:33:03 +0100

    add: posts folder to organize views

---
+++ resources/views/posts/createModal.blade.php
@@ -0,0 +1,122 @@
+<!-- Create post modal -->
+<div class="postModal position-fixed top-50 start-50 translate-middle rounded-3 mt-5" id="postModal">
+        
+    <div class="modal-dialog modal-dialog-centered">
+
+        <div class="modal-content modalContent">
+
+            <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+
+                @csrf
+
+                <!-- Modal header -->
+                <div class="modal-header mx-4 my-3">
+                    <h5 class="modal-title">Create Post</h5>
+                    <button type="button" onclick="closePostModal()" class="btn-close"><i class="bi bi-x-lg"></i></button>
+                </div>
+                
+                <!-- Modal body -->
+                <div class="modal-body px-4 my-2">
+                    
+                    <label for="title">
+                        Title <b class="text-danger">*</b>
+                        @error('title')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </label> 
+
+                    <!-- Blog title -->
+                    <div class="input-group mt-2 mb-3">
+                        <span class="input-group-text" id="addon-wrapping">
+                            <i class="bi bi-card-heading"></i>
+                        </span>
+                        <input type="text" class="postModalValue form-control py-2" name="title" value="{{ old('title') }}" placeholder="Add blog title">
+                    </div>
+                    
+                    <!-- Blog category wrapper -->
+                    <div class="d-flex justify-content-between align-items-center">
+                        <label for="category" class="mt-3">
+                            Category <b class="text-danger">*</b>
+                            @error('category')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </label> 
+                        
+                        <!-- Add category button -->
+                        <span>
+                            <a href="{{ url('/categories') }}" target="_blank" class="addCatBtn btn btn-sm btn-outline-primary">
+                                Add category <i class="bi bi-box-arrow-up-right ms-1"></i>
+                            </a>
+                        </span>
+                    </div>
+
+                    <!-- Category select list -->
+                    <div class="input-group mt-2 mb-3">
+                        <span class="input-group-text rounded-0 rounded-start border-end-0" id="addon-wrapping">
+                            <i class="bi bi-list"></i>
+                        </span>
+
+                        <select class="form-select py-2" name="category" id="floatingSelect" aria-label="Floating label select example">
+                            <option>Select a category</option>
+                            {{-- @foreach($categories as $category)
+                                <option value="{{ $category->id }}" @if(old('category') == $category->id) selected @endif>{{ $category->name }}</option>
+                            @endforeach --}}
+                        </select>
+                    </div>
+                    
+                    <label for="description">
+                        Description <b class="text-danger">*</b>
+                        @error('description')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </label> 
+
+                    <!-- Blog description -->
+                    <div class="input-group mt-2 mb-3">
+                        <span class="input-group-text rounded-0 rounded-start border-end-0" id="addon-wrapping">
+                            <i class="bi bi-pencil-square"></i>
+                        </span>
+                        <textarea class="postModalValue form-control rounded-0 rounded-end decriptionField" name="description" placeholder="Add blog description" id="floatingTextarea2" style="height: 100px">{{ old('description') }}</textarea>
+                    </div>
+                    
+                    <!-- Blog photo -->
+                    <label for="photo">
+                        Blog photo<b class="text-danger"> * </b>
+                        @error('photo')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </label> 
+
+                    <!-- Blog photo upload -->
+                    <div class="input-group mt-2 mb-3">
+                        <span class="input-group-text rounded-0 rounded-start border-end-0" id="addon-wrapping">
+                            <i class="bi bi-card-image"></i>
+                        </span>
+                        <input type="file" class="postModalValue form-control" name="image_path" value="{{ old('image_path') }}" aria-describedby="inputGroupFileAddon01" aria-label="Upload">
+                    </div>
+
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer m-4">
+                    <button type="submit" id="submitBtn" onclick="submitPost()" name="insertPostData" class="sendPostBtn btn btn-primary">
+                        Post <i class="bi bi-send"></i>
+                    </button>
+                </div>
+
+            </form>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Keeps Create Post Modal open when there is error after form submission -->
+@if ($errors->any())
+    <script defer>
+        $(document).ready(function(){
+            openPostModal();
+        });
+    </script>
+@endif
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,148 @@
+@extends('layouts.base')
+
+<!-- Style/title for index page -->
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('frontend_assets/styles/css/index.css') }}">
+    <title>{{ 'Blogger | Home' }}</title>
+@endsection
+
+@section('content')
+
+    <!-- Index page container/wrapper -->
+    <section class="blogContents d-flex justify-content-between">
+
+        <section class="mainContentContainer">   
+
+            @foreach ($posts as $post)
+
+                <div class="postCard border rounded-top rounded-2"> 
+            
+                    <div class="postHeader">
+                        
+                        <div class="userAvater">
+                            <img src="{{ ('frontend_assets/assets/images/moji.png') }}" alt="">
+                            <p class="avaterDetails ms-3"> {{ $post->user->name }} </p>
+                        </div>
+                            
+                        <div class="postInfo">
+            
+                            <p>{{ $post->created_at->diffForHumans() }}</p>
+                            {{-- <p>{{ date('jS M Y', strtotime($post->updated_at)) }}</p> --}}
+            
+                            <i class="bi bi-three-dots-vertical" data-bs-toggle="modal" data-bs-target="#moreInfoModal"></i>
+                            
+                            <!-- More info modal goes here -->
+            
+                        </div>
+            
+                    </div>
+            
+                    <div class="postPhoto">
+            
+                        <!-- Post photo -->
+                        <a href="{{ url('posts.postDetails') }}">
+                            <img src="{{ asset('frontend_assets/assets/images/parisOne.jfif') }}" alt="user account">
+                        </a>
+            
+                        <!-- Post: like, comment, share icons container/wrapper -->
+                        <div class="postIconsContainer d-flex justify-content-end">
+                            
+                            <div class="rightIconsDiv d-flex flex-column justify-content-between align-items-center">
+                                
+                                <!-- Like icon -->
+                                <div class="likes">
+                                    <i id="heart-icon" class="bi bi-heart" onclick="replaceLikeIcon(this)"></i>
+                                    <p>221.9k</p>
+                                </div>
+            
+                                <!-- Comment icon -->
+                                <div class="comments">
+                                    <i class="bi bi-chat-square"></i>
+                                    <p>1907</p>
+                                </div>
+            
+                                <!-- Share icon -->
+                                <div class="shares">
+                                    <i class="bi bi-reply"></i>
+                                    <p>1805</p>
+                                </div>
+            
+                            </div>
+            
+                        </div>
+            
+                    </div>
+            
+                    <!-- Post category & description container/wrapper -->
+                    <div class="postTextWrapper">
+                        <p class="postCategory"> {{ $post->category }} </p>
+                        <p class="postTitle"> {{ Str::limit($post->title, 66) }} </p>
+                        <p class="postParagraph"> {{ Str::limit($post->description, 149) }} </p>
+                    </div>
+            
+                    <!-- Post comment container/wrapper -->
+                    <div class="postCommentWrapper d-flex align-items-center align-items-center">
+            
+                        <!-- Post smiley wrapper -->
+                        <div class="emojiWrapper">
+                            <i class="bi bi-emoji-smile"></i>
+                        </div>
+            
+                        <!-- Post comment textarea wrapper -->
+                        <div class="commentWrapper">
+                            <textarea name="comment" onclick="autoResizeTextarea()" id="expandable-textarea" placeholder="Add a comment..."></textarea>
+                        </div>
+            
+                        <!-- Post button wrapper -->
+                        <div class="postBtnWrapper d-flex justify-content-end">
+                            <button id="postBtnWrapper" type="submit">Post</button>
+                        </div>
+            
+                    </div>
+            
+                </div>
+
+            @endforeach
+
+            {{ $posts->links() }}
+
+        </section>
+
+        <!-- Empty right side section -->
+        <section class="rightSideContentContainer border-start"> 
+            
+        </section>
+
+        <!-- Create post Modal -->
+        @include('posts.createModal')
+        
+    </section>
+
+@endsection
+
+<!-- Create post modal button -->
+<button type="button" onclick="openPostModal()" class="postBtn border-0">
+    <img src="{{ asset('frontend_assets/assets/svg/feather.svg') }}" alt="Click to post">
+</button>
+
+
+@if (Auth::check())
+
+    <!-- Animated tooltip for authenticated user -->
+    <div id="createPostTooltip" class="createPostTooltip">
+        <p>Click to post</p>
+    </div>
+
+    @else
+    
+    <!-- Animated tooltip for unauthenticated user -->
+    <div id="createPostTooltip" class="createPostTooltip">
+        <a href="{{ url('/login') }}">Click to post</a>
+    </div>
+@endif
+
+
+<!-- Black background behind Post Modal -->
+<div id="postModalBackground" onclick="closePostModal()" class="postModalBackground"> 
+
+</div>

6a30092e70ed6565b2d6d810205f07e9ef9f6f46
tobidosumu <dosunmumishael@gmail.com>
2023-02-16 23:58:05 +0100

    fix: pagination styling issue

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -108,7 +108,23 @@
 
             @endforeach
 
-            {{ $posts->links() }}
+
+            {{-- <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li> --}}
+                    {{ $posts->links() }}
+
+                    {{-- <li class="page-item">
+                        <a class="page-link" href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                  </li>
+                </ul>
+            </nav> --}}
 
         </section>
 

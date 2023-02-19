448f01b70346ec2e27c5329e212e2afe9cfdf974
tobidosumu <dosunmumishael@gmail.com>
2023-02-19 21:28:51 +0100

    refactor: admin dashboard base layout view

--- resources/views/admin_layouts/header.blade.php
+++ resources/views/admin_layouts/header.blade.php
@@ -1,30 +1,23 @@
 <!-- Default admin header -->
 <header class="d-flex justify-content-between align-items-center px-5 border-bottom">
 
-    <div class="leftSection d-flex align-items-center">
-        <select name="seller">
-            <option value="sellers">All Seller</option>
-            <option value="seller2">seller 2</option>
-            <option value="seller3">seller 3</option>
-        </select>
-    </div>
-
-    <div class="centerSection d-flex justify-content-between">
+    <div class="leftSection d-flex justify-content-between">
         <!-- Search -->
         <form action="" method="GET" class="d-flex">
             <i class="bi bi-search"></i>
             <input type="search" name="search" placeholder="...search for anything">
         </form>
-
-        <button>
-            <i class="bi bi-plus"></i>
-            <span>Add Seller</span>
-        </button>
     </div>
 
-    <div class="rightSection d-flex justify-content-between align-items-center">
-        <i class="bi bi-bell-fill"></i>
-        <img src="{{ asset('frontend_assets/assets/images/moji.png') }}" alt="admin photo">
+    <div class="rightSection d-flex align-items-center justify-content-between">
+        <i class="bi bi-bell-fill ps-2"></i>
+        <a href="#" class="d-flex align-items-center">
+            <img src="{{ asset('frontend_assets/assets/images/moji.png') }}" alt="admin photo">
+            <p class="ps-2">
+                Mojisola Badmus
+            </p>
+            <i class="bi bi-chevron-down ps-1"></i>
+        </a>
     </div>
 
 </header>
--- resources/views/admin_layouts/sidebar.blade.php
+++ resources/views/admin_layouts/sidebar.blade.php
@@ -1,11 +1,10 @@
 <aside>
 
     <!-- Logo container/wrapper -->
-    <div class="logoContainer p-4">
+    <div class="logoContainer pt-2 ps-4 pb-5">
         <a href="{{ route('posts.index') }}">
-            <img src="{{ asset('frontend_assets/assets/svg/bloggerLogoWhite.svg') }}" alt="blogger logo">
+            <img src="{{ asset('frontend_assets/assets/svg/bloggerLogoBlack.svg') }}" alt="blogger logo">
         </a>
-        <p class="d-flex justify-content-between pt-2">Admin testing<span>Admin user</span></p>
     </div>
 
     <!-- Nav container/wrapper -->
@@ -14,42 +13,49 @@
         <ul class="d-flex flex-column justify-content-between">
     
             <!-- Home link -->
-            <a class="d-flex home" href="{{ url('posts') }}">
+            <a class="d-flex align-items-center home" href="{{ url('posts') }}">
                 <i class="bi bi-house"></i>
                 <span>Home</span>
             </a>
 
             <!-- Category link -->
-            <a class="category" href="{{ url('/category') }}">
+            <a class="d-flex align-items-center category" href="{{ url('/category') }}">
                 <i class="bi bi-card-list"></i>
                 <span>Category</span>
             </a>
 
             <!-- Shop link -->
-            <a class="shop" href="{{ url('/shop') }}">
+            <a class="d-flex align-items-center shop" href="{{ url('/shop') }}">
                 <i class="bi bi-shop"></i>
                 <span>Seller</span>
             </a>
         
             <!-- Product link -->
-            <a class="product" href="{{ url('/product') }}">
+            <a class="d-flex align-items-center product" href="{{ url('/product') }}">
                 <i class="bi bi-box2"></i>
                 <span>Product</span>
             </a>
 
             <!-- User link -->
-            <a class="user" href="{{ url('/user') }}">
+            <a class="d-flex align-items-center user" href="{{ url('/user') }}">
                 <i class="bi bi-person"></i>
                 <span>User</span>
             </a>
 
             <!-- Report link -->
-            <a class="report" href="{{ url('/report') }}">
+            <a class="d-flex align-items-center report" href="{{ url('/report') }}">
                 <i class="bi bi-flag"></i>
                 <span>Report</span>
             </a>
         
         </ul>
+
+        <p class="logout d-flex justify-content-between align-items-center border-top px-4 py-4 mt-5">
+            Logout
+            <a href="#">
+                <i class="bi bi-toggle-off"></i>
+            </a>
+        </p>
 
     </nav>
 
